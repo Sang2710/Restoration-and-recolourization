bd32d20dcf1a05c52a4a2d13613162e678ea1426
Sang2710 <80399745+Sang2710@users.noreply.github.com>
2021-05-02 10:07:25 +0530

    Update run.py

--- run.py
+++ run.py
@@ -98,5 +98,67 @@ if __name__ == "__main__":
 
     print("Finish Stage 1 ...")
     print("\n")
+    
+    ## Stage 2: Face Detection
+
+    print("Running Stage 2: Face Detection")
+    os.chdir(".././Face_Detection")
+    stage_2_input_dir = os.path.join(stage_1_output_dir, "restored_image")
+    stage_2_output_dir = os.path.join(opts.output_folder, "stage_2_detection_output")
+    if not os.path.exists(stage_2_output_dir):
+        os.makedirs(stage_2_output_dir)
+    stage_2_command = (
+        "python detect_all_dlib.py --url " + stage_2_input_dir + " --save_url " + stage_2_output_dir
+    )
+    run_cmd(stage_2_command)
+    print("Finish Stage 2 ...")
+    print("\n")
+
+    ## Stage 3: Face Restore
+    print("Running Stage 3: Face Enhancement")
+    os.chdir(".././Face_Enhancement")
+    stage_3_input_mask = "./"
+    stage_3_input_face = stage_2_output_dir
+    stage_3_output_dir = os.path.join(opts.output_folder, "stage_3_face_output")
+    if not os.path.exists(stage_3_output_dir):
+        os.makedirs(stage_3_output_dir)
+    stage_3_command = (
+        "python test_face.py --old_face_folder "
+        + stage_3_input_face
+        + " --old_face_label_folder "
+        + stage_3_input_mask
+        + " --tensorboard_log --name "
+        + opts.checkpoint_name
+        + " --gpu_ids "
+        + gpu1
+        + " --load_size 256 --label_nc 18 --no_instance --preprocess_mode resize --batchSize 4 --results_dir "
+        + stage_3_output_dir
+        + " --no_parsing_map"
+    )
+    run_cmd(stage_3_command)
+    print("Finish Stage 3 ...")
+    print("\n")
+
+    ## Stage 4: Warp back
+    print("Running Stage 4: Blending")
+    os.chdir(".././Face_Detection")
+    stage_4_input_image_dir = os.path.join(stage_1_output_dir, "restored_image")
+    stage_4_input_face_dir = os.path.join(stage_3_output_dir, "each_img")
+    stage_4_output_dir = os.path.join(opts.output_folder, "final_output")
+    if not os.path.exists(stage_4_output_dir):
+        os.makedirs(stage_4_output_dir)
+    stage_4_command = (
+        "python align_warp_back_multiple_dlib.py --origin_url "
+        + stage_4_input_image_dir
+        + " --replace_url "
+        + stage_4_input_face_dir
+        + " --save_url "
+        + stage_4_output_dir
+    )
+    run_cmd(stage_4_command)
+    print("Finish Stage 4 ...")
+    print("\n")
+
+    print("All the processing is done. Please check the results.")
 
 
