13535e7b2e3b86f3c04b67f7b5584400e2094073
Sang2710 <80399745+Sang2710@users.noreply.github.com>
2021-06-04 15:05:12 +0530

    Update __init__.py

--- Face_Enhancement/models/__init__.py
+++ Face_Enhancement/models/__init__.py
@@ -1,20 +1,14 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
 
 import importlib
 import torch
 
 
 def find_model_using_name(model_name):
-    # Given the option --model [modelname],
-    # the file "models/modelname_model.py"
-    # will be imported.
+    
     model_filename = "models." + model_name + "_model"
     modellib = importlib.import_module(model_filename)
 
-    # In the file, the class called ModelNameModel() will
-    # be instantiated. It has to be a subclass of torch.nn.Module,
-    # and it is case-insensitive.
+   
     model = None
     target_model_name = model_name.replace("_", "") + "model"
     for name, cls in modellib.__dict__.items():
