146916cbd3c038d1ccc2b72b961909d8347a7c7c
Sang2710 <80399745+Sang2710@users.noreply.github.com>
2021-06-04 15:01:29 +0530

    Update pix2pix_model.py

--- Face_Enhancement/models/pix2pix_model.py
+++ Face_Enhancement/models/pix2pix_model.py
@@ -1,5 +1,4 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
+
 
 import torch
 import models.networks as networks
@@ -20,7 +19,7 @@ class Pix2PixModel(torch.nn.Module):
 
         self.netG, self.netD, self.netE = self.initialize_networks(opt)
 
-        # set loss functions
+        
         if opt.isTrain:
             self.criterionGAN = networks.GANLoss(opt.gan_mode, tensor=self.FloatTensor, opt=self.opt)
             self.criterionFeat = torch.nn.L1Loss()
@@ -29,10 +28,6 @@ class Pix2PixModel(torch.nn.Module):
             if opt.use_vae:
                 self.KLDLoss = networks.KLDLoss()
 
-    # Entry point for all calls involving forward pass
-    # of deep networks. We used this approach since DataParallel module
-    # can't parallelize custom functions, we branch to different
-    # routines based on |mode|.
     def forward(self, data, mode):
         input_semantics, real_image, degraded_image = self.preprocess_input(data)
 
@@ -76,9 +71,7 @@ class Pix2PixModel(torch.nn.Module):
         if self.opt.use_vae:
             util.save_network(self.netE, "E", epoch, self.opt)
 
-    ############################################################################
-    # Private helper methods
-    ############################################################################
+  
 
     def initialize_networks(self, opt):
         netG = networks.define_G(opt)
@@ -94,13 +87,9 @@ class Pix2PixModel(torch.nn.Module):
 
         return netG, netD, netE
 
-    # preprocess the input, such as moving the tensors to GPUs and
-    # transforming the label map to one-hot encoding
-    # |data|: dictionary of the input data
 
     def preprocess_input(self, data):
-        # move to GPU and change data types
-        # data['label'] = data['label'].long()
+      
 
         if not self.opt.isTrain:
             if self.use_gpu():
@@ -108,19 +97,13 @@ class Pix2PixModel(torch.nn.Module):
                 data["image"] = data["image"].cuda()
             return data["label"], data["image"], data["image"]
 
-        ## While testing, the input image is the degraded face
+        
         if self.use_gpu():
             data["label"] = data["label"].cuda()
             data["degraded_image"] = data["degraded_image"].cuda()
             data["image"] = data["image"].cuda()
 
-        # # create one-hot label map
-        # label_map = data['label']
-        # bs, _, h, w = label_map.size()
-        # nc = self.opt.label_nc + 1 if self.opt.contain_dontcare_label \
-        #     else self.opt.label_nc
-        # input_label = self.FloatTensor(bs, nc, h, w).zero_()
-        # input_semantics = input_label.scatter_(1, label_map, 1.0)
+        )
 
         return data["label"], data["image"], data["degraded_image"]
 
@@ -189,8 +172,7 @@ class Pix2PixModel(torch.nn.Module):
 
         return fake_image, KLD_loss
 
-    # Given fake and real image, return the prediction of discriminator
-    # for each fake and real image.
+
 
     def discriminate(self, input_semantics, fake_image, real_image):
 
@@ -201,10 +183,7 @@ class Pix2PixModel(torch.nn.Module):
             fake_concat = torch.cat([input_semantics, fake_image], dim=1)
             real_concat = torch.cat([input_semantics, real_image], dim=1)
 
-        # In Batch Normalization, the fake and real images are
-        # recommended to be in the same batch to avoid disparate
-        # statistics in fake and real images.
-        # So both fake and real images are fed to D all at once.
+       
         fake_and_real = torch.cat([fake_concat, real_concat], dim=0)
 
         discriminator_out = self.netD(fake_and_real)
@@ -213,10 +192,8 @@ class Pix2PixModel(torch.nn.Module):
 
         return pred_fake, pred_real
 
-    # Take the prediction of fake and real images from the combined batch
     def divide_pred(self, pred):
-        # the prediction contains the intermediate outputs of multiscale GAN,
-        # so it's usually a list
+        
         if type(pred) == list:
             fake = []
             real = []
